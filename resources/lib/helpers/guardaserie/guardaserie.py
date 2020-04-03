a05c902794946fcb7b2a51c1b3d548190fae5293
Luca <luca.garavaglia.lug@gmail.com>
2020-04-03 20:04:29 +0200

    update domini

--- resources/lib/helpers/guardaserie/guardaserie.py
+++ resources/lib/helpers/guardaserie/guardaserie.py
@@ -5,7 +5,7 @@ import time
 
 class GuardaSerie():
     def __init__(self):
-        self.domain = 'http://guardaserie.productions/'
+        self.domain = 'http://guardaserie.digital/'
         self.search_url = '{0}?s={1}'
         self.cf_session = None
     
--- resources/lib/helpers/hdcloud/altadefinizione.py
+++ resources/lib/helpers/hdcloud/altadefinizione.py
@@ -4,7 +4,7 @@ from resources.lib.streaming_hosts.hdload import HDLoad
 
 class Altadefinizione():
     def __init__(self):
-        self.domain = 'https://altadefinizione.style/'
+        self.domain = 'https://altadefinizione.rocks/'
         self.search_url = '{0}?s={1}'
 
     def get_playable_url(self, title, hd_iframe, player_name="vidoza"):
