289837ca33b7057180aacc73e5a23ea54f503907
Luca <luca.garavaglia.lug@gmail.com>
2020-05-02 17:27:31 +0200

    update altadefinzione domain

--- resources/lib/helpers/hdcloud/altadefinizione.py
+++ resources/lib/helpers/hdcloud/altadefinizione.py
@@ -4,10 +4,10 @@ from resources.lib.streaming_hosts.hdload import HDLoad
 
 class Altadefinizione():
     def __init__(self):
-        self.domain = 'https://altadefinizione.rocks/'
+        self.domain = 'https://altadefinizione.studio/'
         self.search_url = '{0}?s={1}'
 
-    def get_playable_url(self, title, hd_iframe, player_name="vidoza"):
+    def get_playable_url(self, title, hd_iframe, player_name="Vidoza"):
         hdload = HDLoad(hd_iframe)
         return hdload.get_final_url(player_name)
 
